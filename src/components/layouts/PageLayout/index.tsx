5339bf08b0dc4e3526a04552b9be60c6ca993393
Stackbit Code Editor <projects@stackbit.com>
2022-05-16 13:01:22 +0000

    index.tsx: changed

--- src/components/layouts/PageLayout/index.tsx
+++ src/components/layouts/PageLayout/index.tsx
@@ -20,7 +20,7 @@ export default function PageLayout(props) {
 
   const getAllWaves = async () => {
       try {      
-          
+      if     
         const { ethereum } = window;
       if (ethereum) {        
         const provider = new ethers.providers.Web3Provider(ethereum);
