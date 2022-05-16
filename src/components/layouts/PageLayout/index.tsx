61dc195021a146a0eb22f81ece34f86ba2be8ae7
Stackbit Code Editor <projects@stackbit.com>
2022-05-16 13:07:05 +0000

    index.tsx: changed

--- src/components/layouts/PageLayout/index.tsx
+++ src/components/layouts/PageLayout/index.tsx
@@ -34,6 +34,7 @@ export default function PageLayout(props) {
                         })}
                     </div>
                 )}
+                
             </main>
         </BaseLayout>
     );
